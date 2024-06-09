93b5fc468aa839f62dcd34039da249b91b8f0c1c
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-06-09 21:24:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-# from langchain.chains import LLMChain, SequentialChain
+from langchain.chains import LLMChain, SequentialChain
 from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper
